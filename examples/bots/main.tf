7c040cedf88ed1af0c5cf42372923b3a812d9693
Sergey Novikov <sergey.novikov@flaconi.de>
2024-04-22 17:38:21 +0200

    OPS-5899 Add examples

---
+++ examples/bots/main.tf
@@ -0,0 +1,43 @@
+module "rulesets" {
+  source    = "./../../"
+  api_token = var.api_token
+  domain    = var.domain
+  name    = "default"
+  kind    = "zone"
+  phase   = "http_request_firewall_custom"
+  rules = [
+    {
+      description = "User-Agent: skip"
+      enabled     = true
+      action      = "skip"
+      expression  = <<-EOT
+      (http.user_agent contains "Bot/" and http.request.uri.path eq "/api/v1")
+      EOT
+      products    = ["waf"]
+    },
+    {
+      description = "User-Agent: log"
+      enabled     = false
+      action      = "log"
+      expression  = <<-EOT
+      (http.user_agent contains "Bot/" and http.request.uri.path eq "/api/v1")
+      EOT
+      products    = []
+    },
+    {
+      description = "Bots: log"
+      action      = "log"
+      expression  = <<-EOT
+      (cf.bot_management.score eq 2)
+      EOT
+    },
+    {
+      description = "Bots: challenge"
+      enabled     = false
+      action      = "managed_challenge"
+      expression  = <<-EOT
+      (cf.bot_management.score eq 9)
+      EOT
+    },
+  ]
+}
---
+++ examples/bots/outputs.tf
@@ -0,0 +1,4 @@
+output "records" {
+  description = "Cloudflare Zone DNS Records"
+  value       = module.rulesets.rules
+}
