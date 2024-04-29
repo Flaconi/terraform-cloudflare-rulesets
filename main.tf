25ce376ab8d8ec37ad1e65868c3b2f11fc521dde
Volkan Selcuk <vselcuk@gmail.com>
2024-04-29 13:28:54 +0200

    PLT-854 added support for the following phases: - http_config_settings - http_log_custom_fields - http_request_dynamic_redirect - http_request_origin

--- main.tf
+++ main.tf
@@ -6,15 +6,39 @@ resource "cloudflare_ruleset" "this" {
   description = var.description
 
   dynamic "rules" {
-    for_each = local.rules
+    for_each = var.rules
 
     content {
       action = rules.value.action
       dynamic "action_parameters" {
         for_each = rules.value.action_parameters[*]
         content {
+          # http_request_origin
+          host_header = action_parameters.value.host_header
+
+          # http_config_settings
+          polish = action_parameters.value.polish
+
+          # http_request_dynamic_redirect
+          dynamic "from_value" {
+            for_each = rules.value.action_parameters.from_value[*]
+            content {
+              preserve_query_string = from_value.value.preserve_query_string
+              status_code           = from_value.value.status_code
+              target_url {
+                value = from_value.value.target_url.value
+              }
+            }
+          }
+
+          # http_request_firewall_custom
           ruleset  = action_parameters.value.ruleset
           products = action_parameters.value.products
+
+          # http_log_custom_fields
+          cookie_fields   = action_parameters.value.cookie_fields
+          request_fields  = action_parameters.value.request_fields
+          response_fields = action_parameters.value.response_fields
         }
       }
       description = rules.value.description
