520db9309fadff8e1f8133d08b16ed7f6adeb559
Volkan Selcuk <vselcuk@gmail.com>
2025-11-07 14:40:11 +0100

    PLT-0 set default ttl to optional for cache rulesets

--- variables.tf
+++ variables.tf
@@ -65,7 +65,7 @@ variable "rules" {
       # phase: http_request_cache_settings
       cache = optional(bool)
       edge_ttl = optional(object({
-        default = number
+        default = optional(number)
         mode    = string
         status_code_ttl = optional(list(object({
           value       = number
