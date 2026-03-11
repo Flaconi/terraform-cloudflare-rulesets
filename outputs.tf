8bfc9883ae203b59e34c1828f7c3787ba317a190
Volkan Selcuk <vselcuk@gmail.com>
2026-03-11 16:00:07 +0100

    PLT-1469 removed the outputs since there is no use-case

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "zone" {
-  description = "Current zone information."
-  value       = { for k, v in data.cloudflare_zones.this.result[0] : k => v if k != "development_mode" }
-}
-
-output "rules" {
-  description = "Created Cloudflare rules for the current zone."
-  value       = cloudflare_ruleset.this.rules
-}
